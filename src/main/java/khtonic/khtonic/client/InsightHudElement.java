2631dd686abc124d7162aa42d94137d8143e7dfe
Lev Ostapenko <012938576a@gmail.com>
2023-02-09 23:10:13 +0300

    finished hud class

--- src/main/java/khtonic/khtonic/client/InsightHudElement.java
+++ src/main/java/khtonic/khtonic/client/InsightHudElement.java
@@ -1,5 +1,22 @@
 package khtonic.khtonic.client;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import khtonic.khtonic.Khtonic;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+
 public class InsightHudElement {
-    private 
+    private static final ResourceLocation INSIGHT = new ResourceLocation(Khtonic.MODID,
+            "textures/insight/insight.png");
+    public static final IGuiOverlay INSIGHT_HUD = ((gui, poseStack, partialTick, screenWidth, screenHeight) -> {
+       int x = screenWidth/2;
+       int y = screenHeight;
+
+       RenderSystem.setShader(GameRenderer::getPositionTexShader);
+       RenderSystem.setShaderColor(1.0F, 1.0F,1.0F, 1.0F);
+       RenderSystem.setShaderTexture(0, INSIGHT);
+        GuiComponent.blit(poseStack,x-94, y-54, 0,0,12,12,12,12);
+    });
 }
